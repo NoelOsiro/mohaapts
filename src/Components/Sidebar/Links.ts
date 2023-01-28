3395db87a5419bf7f91e09f0b4d266ed679550e6
NoelOsiro <noel.osiro@azubiafrica.org>
2023-01-28 21:34:21 +0300

    Finished Sidebar

--- src/Components/Sidebar/Links.ts
+++ src/Components/Sidebar/Links.ts
@@ -6,38 +6,45 @@ import { FcBiotech, FcMoneyTransfer, FcSettings } from "react-icons/fc";
 
 
 export interface ILink{
+    id:number;
     href:string;
     icon:IconType;
     text:string;
 }
 
 export const SidebarLinks:ILink[]=[
-    {
+    {   
+        id:1,
         href:"#",
         icon:MdSpaceDashboard,
         text: "Dashboard"
     },
     {
+        id:2,
         href:"#",
         icon:RiHomeSmileLine,
         text: "Property"
     },
     {
+        id:3,
         href:"#",
         icon:FaHouseUser,
         text: "My Tenants"
     },
-    {
+    {   
+        id:4,
         href:"#",
         icon:FcBiotech,
         text: "Analytics"
     },
     {
+        id:5,
         href:"#",
         icon:FcMoneyTransfer,
         text: "Payments"
     },
     {
+        id:6,
         href:"#",
         icon:FcSettings,
         text: "Services"
